0219d73e16b163c2ac11d1f6a9d35d57050ccde5
Dieimp <mraingue@gmail.com>
2025-08-08 17:16:57 -0300

    feature/initial-commit

---
+++ PersonalLocker/PersonalLocker/View/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using PersonalLocker.ViewModel;
+using System.Windows;
+
+
+namespace PersonalLocker
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
+        }
+    }
+}
---
+++ PersonalLocker/PersonalLocker/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using PersonalLocker.Helpers;
+using PersonalLocker.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace PersonalLocker.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        private HelloWorldModel _model;
+        public MainWindowViewModel()
+        {
+            _model = new HelloWorldModel();
+            ShowMessageCommand = new RelayCommand(ShowMessage);
+        }
+
+        public ICommand ShowMessageCommand { get; }
+
+        private void ShowMessage()
+        {
+            // O ViewModel chama a lógica de negócio do Model
+            string message = _model.GetMessage();
+
+            // A exibição de um MessageBox é uma responsabilidade da View,
+            // mas para um exemplo simples, podemos fazê-lo aqui.
+            // Em uma aplicação real, seria preferível usar um serviço de diálogo.
+            MessageBox.Show(message);
+        }
+
+        
+      
+    }
+}

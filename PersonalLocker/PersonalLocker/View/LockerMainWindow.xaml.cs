2ac90cee159d9a9cebd7237ecc23081615cce8b8
Dieimp <mraingue@gmail.com>
2025-08-11 17:40:07 -0300

    criacao basica se o caminho do  arquivo existe

---
+++ PersonalLocker/PersonalLocker/View/LockerMainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using PersonalLocker.ViewModel;
+using System.Windows;
+
+namespace PersonalLocker
+{
+
+    public partial class LockerMainWindow : Window
+    {
+        public LockerMainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new LockerMainWindowViewModel();
+        }
+    }
+}
